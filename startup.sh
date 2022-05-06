72b57ed6c06a93b08b2310b799bc98161e6b4372
Paul Hagstrom <hagstrom@bu.edu>
2022-05-06 05:28:40 +0000

    Update startup script to start x11vnc with Xvfb

--- startup.sh
+++ startup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-Xvfb :1 &
-DISPLAY=:1
-export DISPLAY
-/usr/bin/x11vnc -display :1 -bg -nopw -xkb -listen localhost &
-fluxbox &
+#Xvfb :1 &
+#DISPLAY=:1
+#export DISPLAY
+#/usr/bin/x11vnc -display :1 -forever -localhost -nopw -bg -xkb
+/usr/bin/x11vnc -create -env FD_PROG=/usr/bin/fluxbox -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 -env X11VNC_CREATE_GEOM={1:-1024x768x16} -gone 'killall Xvfb' -bg -nopw -forever
+#fluxbox &
 node index.js
