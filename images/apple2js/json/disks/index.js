a3d194d2e7fc88eeae1863acee8213a63657bfef
Paul Hagstrom <hagstrom@bu.edu>
2022-05-07 12:35:58 -0400

    Precompute disks and link them into apple2js

---
+++ images/apple2js/json/disks/index.js
@@ -0,0 +1,22 @@
+disk_index = [
+  {
+    "filename": "json/disks/ugwp.json",
+    "name": "Untitled Word Game Pro",
+    "category": "Game"
+  },
+  {
+    "filename": "json/disks/dos33master.json",
+    "name": "DOS 3.3 Master",
+    "category": "System"
+  },
+  {
+    "filename": "json/disks/prodos.json",
+    "name": "ProDOS",
+    "category": "System"
+  },
+  {
+    "filename": "json/disks/audit.json",
+    "name": "Apple II Audit",
+    "category": "Utility"
+  }
+];
--- images/apple2js/startup.sh
+++ images/apple2js/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-./bin/dsk2json -c "Game" -n "Untitled Word Game Pro" ./disks/ugwp.dsk > ./json/disks/ugwp.json
-./bin/index
+# this was not working right so for now I just link in a pre-prepared json disk list
+#./bin/dsk2json -c "Game" -n "Untitled Word Game Pro" ./disks/ugwp.dsk > ./json/disks/ugwp.json
+#./bin/index
 ./node_modules/http-server/bin/http-server
