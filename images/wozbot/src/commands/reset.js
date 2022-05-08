3d68e58d33491a9afe7c0de9e535639b08ca0443
Paul Hagstrom <hagstrom@bu.edu>
2022-05-07 22:21:32 -0400

    Defer reset reply in case it takes too long

--- images/wozbot/src/commands/reset.js
+++ images/wozbot/src/commands/reset.js
@@ -1,22 +1,18 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-// Allow spawning so we can launch the emulator in the container
-// const { exec } = require('child_process');
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-// const Keyv = require('keyv');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('reset')
 		.setDescription('(Re)boots the machine!'),
 	async execute(interaction) {
-		// const keyv = new Keyv();
-		// const browserWSEndpoint = await keyv.get('browserWSEndpoint');
+		await interaction.deferReply();
 		const browserWSEndpoint = fs.readFileSync('/tmp/a2js-ws');
 		const browser = await puppeteer.connect({ browserWSEndpoint });
 		const pages = await browser.pages();
 		const page = pages[0];
 		await page.goto('http://apple2js:8080/apple2js.html#ugwp');
-		await interaction.reply('CHUNKA CHUNKA CHUNKA!');
+		await interaction.editReply('CHUNKA CHUNKA CHUNKA!');
 	},
 };
