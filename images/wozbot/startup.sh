e70393a25f62d6b2b56787fb3f520dbbe217c948
Paul Hagstrom <hagstrom@bu.edu>
2022-05-08 02:48:16 -0400

    Add startup script for wozbot

---
+++ images/wozbot/startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/x11vnc -create -env FD_PROG=/usr/bin/fluxbox -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 -env X11VNC_CREATE_GEOM={1:-1024x768x16} -gone 'killall Xvfb' -bg -nopw -forever
+node index.js
