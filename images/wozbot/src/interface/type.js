009bd4f30bf65ee33e8f4c9c4fa1dca9a397cd6d
Paul Hagstrom <hagstrom@bu.edu>
2022-05-08 23:11:18 -0400

    Fix minor indenting oddity

--- images/wozbot/src/interface/type.js
+++ images/wozbot/src/interface/type.js
@@ -3,18 +3,18 @@ const { execSync } = require('child_process');
 
 module.exports = {
 	async sendLine(line) {
-			execSync(`xdotool search --name Apple type --delay 100 ${line}`, (error, stdout, stderr) => {
-				if (error) {
-					console.log(`error: ${error.message}`);
-					return;
-				}
-				if (stderr) {
-					console.log(`stderr:${stderr}`);
-					return;
-				}
-				console.log(`stdout: ${stdout}`);
-			});
-    },
+		execSync(`xdotool search --name Apple type --delay 100 ${line}`, (error, stdout, stderr) => {
+			if (error) {
+				console.log(`error: ${error.message}`);
+				return;
+			}
+			if (stderr) {
+				console.log(`stderr:${stderr}`);
+				return;
+			}
+			console.log(`stdout: ${stdout}`);
+		});
+  },
   async sendReturn() {
     execSync('xdotool search --name Apple key Return', (error, stdout, stderr) => {
 			if (error) {
