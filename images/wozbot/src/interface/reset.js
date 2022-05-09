d354e548c6e2a9211fa267c205fa5d41a0593079
Paul Hagstrom <hagstrom@bu.edu>
2022-05-08 22:51:53 -0400

    Try synchronous functions in reset

--- images/wozbot/src/interface/reset.js
+++ images/wozbot/src/interface/reset.js
@@ -1,28 +1,33 @@
 const { execSync } = require('child_process');
+// synchronous because I am old
+
+function stopEmulator() {
+  console.log('Stopping emulator.');
+	execSync('pkill -f izapple2sdl_linux', (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+		}
+		if (stderr) {
+			console.log(`stderr:${stderr}`);
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+}
+
+function startEmulator() {
+  console.log('Starting emulator.');
+	execSync('/usr/src/emulator/izapple2sdl_linux /usr/src/bot/disks/uwgp.dsk &', (error, stdout, stderr) => {
+		if (error) {
+			console.log(`error: ${error.message}`);
+		}
+		if (stderr) {
+			console.log(`stderr:${stderr}`);
+		}
+		console.log(`stdout: ${stdout}`);
+	});
+}
 
 module.exports = {
-	async stopEmulator() {
-    console.log('Stopping emulator.');
-		execSync('pkill -f izapple2sdl_linux', (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-			}
-			if (stderr) {
-				console.log(`stderr:${stderr}`);
-			}
-			console.log(`stdout: ${stdout}`);
-		});
-  },
-  async startEmulator() {
-    console.log('Starting emulator.');
-		execSync('/usr/src/emulator/izapple2sdl_linux /usr/src/bot/disks/uwgp.dsk &', (error, stdout, stderr) => {
-			if (error) {
-				console.log(`error: ${error.message}`);
-			}
-			if (stderr) {
-				console.log(`stderr:${stderr}`);
-			}
-			console.log(`stdout: ${stdout}`);
-		});
-	},
-};
+  startEmulator,
+  stopEmulator
+}
