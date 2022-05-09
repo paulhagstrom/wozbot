616c3ebae62a12fdeb568696a2f73094bcec10b2
Paul Hagstrom <hagstrom@bu.edu>
2022-05-09 12:00:47 -0400

    See if I can do a screen update after

--- images/wozbot/src/commands/show.js
+++ images/wozbot/src/commands/show.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MessageAttachment, MessageEmbed } = require('discord.js');
 const { recordScreen } = require('../interface/show.js');
+const wait = require('node:timers/promises').setTimeout;
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -19,5 +20,18 @@ module.exports = {
 			files: [attvid]
 		});
 		console.log('Reply has gone out.');
+		// wait five seconds and then resample the screen and replace the reply
+		wait(5000);
+		await recordScreen();
+		const attvid2 = new MessageAttachment('/tmp/screen.gif');
+		const replyEmbed2 = new MessageEmbed()
+			// .setTitle('Current screen')
+			.setImage('attachment://screen.gif');
+		console.log('Edit reply is going out.');
+		await interaction.editReply({
+			embeds: [replyEmbed2],
+			files: [attvid2]
+		});
+		console.log('Edit reply has gone out.');
 	},
 };
